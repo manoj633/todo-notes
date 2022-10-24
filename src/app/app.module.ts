2f461cc5469e587c4ddbbbf718922ac701382f1f
Manoj <manoj09633@gmail.com>
2022-10-24 07:56:43 +0530

    components and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoFormComponent,
+    TodoComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
